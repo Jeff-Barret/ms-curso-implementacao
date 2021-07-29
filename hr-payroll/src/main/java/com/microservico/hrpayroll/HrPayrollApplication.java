3b4cb2aa6704b963145f074e403d2d60dc0b7855
Jeff-Barret <jeferson.b_nascimento@hotmail.com>
2021-07-29 10:07:38 -0300

    Ribbon load balancing

--- hr-payroll/src/main/java/com/microservico/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/microservico/hrpayroll/HrPayrollApplication.java
@@ -2,8 +2,10 @@ package com.microservico.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@RibbonClient (name = "hr-worker")
 @EnableFeignClients //Feramenta utilizada para conversar entre projetos e o Feign já é integrado com outras ferramentas do cloud spring
 @SpringBootApplication
 public class HrPayrollApplication {
--- hr-worker/src/main/java/com/microservico/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/microservico/hrworker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.microservico.hrworker.resources;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +20,12 @@ import com.microservico.hrworker.repositories.WorkerRepository;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
+	
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Autowired
+	private Environment env;
+	
 
 	@Autowired //É a forma que voce faz a injeção de dependencias usando framework
 	private WorkerRepository repository;
@@ -29,6 +38,9 @@ public class WorkerResource {
 	
 	@GetMapping (value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+		
+		logger.info("PORT = " + env.getProperty("local.server.port"));
+		
 		Worker obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
 	}
