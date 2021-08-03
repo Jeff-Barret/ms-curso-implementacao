615a6b595f92347cbde85f524afa3e0d7234b62f
Jeff-Barret <jeferson.b_nascimento@hotmail.com>
2021-08-03 11:06:27 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/microservico/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/microservico/hrpayroll/HrPayrollApplication.java
@@ -2,9 +2,11 @@ package com.microservico.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableCircuitBreaker
 @EnableEurekaClient
 @EnableFeignClients //Feramenta utilizada para conversar entre projetos e o Feign já é integrado com outras ferramentas do cloud spring
 @SpringBootApplication
